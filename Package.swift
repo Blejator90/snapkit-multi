cc67fcd1ee70bda843478541dbed5ae92899159a
Nebojsa Nadj <nebojsanadj@icloud.com>
2021-01-05 15:07:12 +0100

    update package file

--- Package.swift
+++ Package.swift
@@ -9,21 +9,18 @@ import PackageDescription
 
 let package = Package(
     name: "snapkit-multi",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "snapkit-multi",
-            targets: ["snapkit-multi"]),
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_11),
+        .tvOS(.v10)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "snapkit-multi", targets: ["snapkit-multi"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "snapkit-multi",
-            dependencies: [])
+        .target(name: "snapkit-multi", path: "Sources"),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
