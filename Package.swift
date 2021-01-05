8ff2c8fcfd63e3cae53c186eabe035c48cf39d3e
Nebojsa Nadj <nebojsanadj@icloud.com>
2021-01-05 14:44:41 +0100

    inital commit with package file

---
+++ Package.swift
@@ -0,0 +1,29 @@
+//
+//  Package.swift
+//  snapkit-multi
+//
+//  Created by Nebojsa Nadj on 5.1.21..
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "snapkit-multi",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "snapkit-multi",
+            targets: ["snapkit-multi"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "snapkit-multi",
+            dependencies: [])
+    ]
+)
---
+++ Sources/snapkit.h
@@ -0,0 +1,18 @@
+//
+//  snapkit_ios.h
+//  snapkit-ios
+//
+//  Created by Nebojsa Nadj on 5.1.21..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for snapkit_ios.
+FOUNDATION_EXPORT double snapkit_iosVersionNumber;
+
+//! Project version string for snapkit_ios.
+FOUNDATION_EXPORT const unsigned char snapkit_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <snapkit_ios/PublicHeader.h>
+
+
